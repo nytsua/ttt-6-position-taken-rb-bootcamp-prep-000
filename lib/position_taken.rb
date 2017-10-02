9bf59ff053687d8ae9a149cbdcde3c0065a96a16
Austyn Trace <atrace@gmail.com>
2017-10-02 00:28:58 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,4 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if index == nil
-    return false
-  end
-  board[index].strip != ""
+  board[index].strip != "" || 
 end
